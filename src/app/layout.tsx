a8a071be7203e1046c9ca8b4bfd06c933a5cbeb6
Arnav Cz <arczyn@Arnavs-MacBook-Air.local>
2025-09-21 21:16:41 +0530

    added analytics to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from "@vercel/analytics/next";
 
 const metadataBase = new URL('https://arnav.web.app');
 
